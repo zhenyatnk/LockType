bc133a64e1483b49c22793dd0550b3ca98ccdcc1
zhenyatnk <tinkalyk@mail.ru>
2015-06-04 05:45:27 -0700

    Add in cmakelist def -std=c++11, remove using boost\shared_ptr

--- intf/DSmartPtr.h
+++ intf/DSmartPtr.h
@@ -1,21 +1,13 @@
 #ifndef _DEF_SMART_PTR_H_
 #define _DEF_SMART_PTR_H_
 
-#if defined(WIN32)
-   #include <memory>
-#elif defined(LINUX)
-   #include <boost/shared_ptr.hpp>
-#endif
+#include <memory>
 
 template <typename Type>
 class SmartPtr
 {
 public:
-#if defined(WIN32)
    typedef typename std::shared_ptr<Type> Ptr;
-#elif defined(LINUX)
-   typedef typename boost::shared_ptr<Type> Ptr;
-#endif    
 };
 
 #endif
